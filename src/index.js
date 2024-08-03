f04b8ac9fbc87ab6a7839d1feba27ccca9cbbeb1
tyler <tylerjyoungdahl@gmail.com>
2024-08-02 18:02:19 -0700

    navbar set up

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import "./assets/styles/style.css";
 
 console.log("welcome to the krusty krab");
+
+// const header = document.getElementById("header");
+// const thing = document.createElement("div");
+// thing.innerHTML = "Roboto";
+// thing.classList.add("element");
+// header.appendChild(thing);
